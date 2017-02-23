73ef304600d1bff1055079ecd90ab324d4a84e7c
Ueli Banholzer <ueli@whatwedo.ch>
2017-02-23 03:14:13 +0100

    fix(listener): prevent listener to trigger itself

--- EventListener/IndexListener.php
+++ EventListener/IndexListener.php
@@ -154,7 +154,10 @@ class IndexListener implements EventSubscriber
             }
         }
 
+        // prevents https://github.com/doctrine/doctrine2/issues/4004
+        $em->getEventManager()->removeEventSubscriber($this);
         $em->flush();
+        $em->getEventManager()->addEventSubscriber($this);
     }
 
     /**
