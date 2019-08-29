728de48d3cdf9b514ba692299915065d5a1d5212
Maurizio Monticelli <maurizio@whatwedo.ch>
2019-08-29 16:42:13 +0200

    pre and post Search hook #13

---
+++ Annotation/Searchable.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace whatwedo\SearchBundle\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+
+
+/**
+ * Class Searchable
+ * @package whatwedo\SearchBundle\Annotation
+ *
+ * @Annotation
+ * @Target({"CLASS"})
+ */
+class Searchable
+{
+
+    /**
+     * @var string
+     */
+    public $preSearch;
+
+    /**
+     * @var string
+     */
+    public $postSearch;
+
+    /**
+     * @return string
+     */
+    public function getPreSearch(): ? string
+    {
+        return $this->preSearch;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostSearch(): ? string
+    {
+        return $this->postSearch;
+    }
+
+
+}
