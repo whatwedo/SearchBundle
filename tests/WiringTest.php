4dc16248a6d288e758b75a74cf1c4bce10568099
Maurizio Monticelli <rugbymauri@gmail.com>
2022-02-03 23:48:16 +0100

    styles: fix styles

--- tests/WiringTest.php
+++ tests/WiringTest.php
@@ -8,12 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use whatwedo\SearchBundle\Manager\FilterManager;
 use whatwedo\SearchBundle\Manager\IndexManager;
 use whatwedo\SearchBundle\Manager\SearchManager;
-use whatwedo\SearchBundle\Tests\App\Helper\ResetDatabase;
-use Zenstruck\Foundry\Test\Factories;
 
 class WiringTest extends KernelTestCase
 {
-
     public function testServiceWiring()
     {
         foreach ([
